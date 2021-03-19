99f31ef0f25022193913eafd20efcc9a2d1fec9f
REEKRAJROY <reekrajroy@gmail.com>
2021-03-19 21:37:20 +0530

    Added speech recognition, wikipedia and smtplib

--- jarvis.py
+++ jarvis.py
@@ -1,5 +1,9 @@
 import pyttsx3 #pip install
 import datetime
+import speech_recognition as sr #pip install SpeechRecognition
+import wikipedia #pip install wikipedia
+import smtplib
+
 engine = pyttsx3.init()
 
 
@@ -27,8 +31,85 @@ def date_():
 #date_() uncomment this to use date_()
 
 def wishme():
-    speak("Welcome back Reekraj Roy!!")
+    speak("Welcome back Reekraj Roy")
     time_()
     date_()
 
-wishme()
+    hour = datetime.datetime.now().hour
+
+    if hour>=6 and hour<12:
+        speak("Good Morning Sir!")
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon Sir!")
+    elif hour>=18 and hour<24:
+        speak("Good Evening Sir!")
+    else:
+        speak("Good Night Sir!")
+
+    speak("Jarvis at your service. Please tell me how can I help you today?")
+
+#wishme()
+def TakeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening.....")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+        try:
+            print("Recognizing.....")
+            query = r.recognize_google(audio,language='en-US')
+            print(query)
+
+        except Exception as e:
+            print(e)
+            print("Say that again please.....")
+            return "None"
+        return query
+
+
+def sendEmail(to,content):
+    server=smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo()
+    server.starttls() #for this function, we must enable low security in gmail 
+
+    server.login('username@gmail.com','password')
+    server.sendmail('username@gmail.com',to,content)
+
+    server.close()
+
+if __name__ == "__main__":
+
+    wishme()
+
+    while True:
+        query = TakeCommand().lower()     #all commands will be stored in lower case in query for easy recognition
+
+        if 'time' in query: #tell us time when asked
+            time_()
+
+        elif 'date' in query: #tell us date when asked
+            date_()
+
+        elif 'wikipedia' in query:
+            speak("Searching.....")
+            query=query.replace('wikipedia','')
+            result=wikipedia.summary(query,sentences=3)
+            speak("According to Wikipedia")
+            print(result)
+            speak(result)
+
+        elif 'send email' in query:
+            try:
+                speak('What should I say?')
+                content=TakeCommand()
+                speak("Who is the receiver?")
+                receiver=input("Enter Receiver's Email :")     #provide receiver's email
+                to=receiver
+                sendEmail(to,content)
+                speak(content)
+                speak('Email has been sent')
+
+            except Exception as e:
+                print(e)
+                speak("Unable to send Email.")
